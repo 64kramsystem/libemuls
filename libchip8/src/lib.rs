8df0cb716c0d016d84223174d97500f1f808735d
Saverio Miroddi <saverio.pub2@gmail.com>
2020-08-05 19:04:09 +0200

    libchip8: Start cycle emulation

--- libchip8/src/lib.rs
+++ libchip8/src/lib.rs
@@ -18,7 +18,9 @@ struct Chip8 {
 }
 
 impl Chip8 {
-    fn new() -> Chip8 {
+    // Simplification: instantiate with the data loaded.
+    //
+    fn new(game_rom: &Vec<Byte>) -> Chip8 {
         Chip8 {
             ram: [0; 4096],
             screen: [0; 64 * 32],
@@ -36,12 +38,18 @@ impl Chip8 {
         }
     }
 
-    fn load_game(&self, game_rom: &[Byte]) {
-        println!("WRITEME: ")
-    }
-
     fn emulate_cycle(&self) {
-        println!("WRITEME: emulate_cycle")
+        println!("WRITEME: emulate_cycle");
+
+        // The decode/execute stages are conventionally split. In this system there is not real need
+        // for this, so, for simplicity, they're merged. A separate-stages design would likely have
+        // a function pointer and the operands as intermediate values.
+        //
+        let instruction = self.cycle_fetch();
+
+        self.cycle_decode_execute(instruction);
+
+        self.cycle_update_timers();
     }
 
     fn draw_flag(&self) -> bool {
@@ -55,6 +63,31 @@ impl Chip8 {
 
     fn set_keys(&self) {
         println!("WRITEME: set_keys")
+    }
+
+    // CYCLE MAIN STAGES ///////////////////////////////////////////////////////////////////////////
+
+    fn cycle_fetch(&self) -> Word {
+        let instruction_hi_byte = self.ram[self.pc as usize] as Word;
+        let instruction_lo_byte = self.ram[(self.pc + 1) as usize] as Word;
+        (instruction_hi_byte << 8) + instruction_lo_byte
+    }
+
+    fn cycle_decode_execute(&self, instruction: Word) {
+        println!("WRITEME: cycle_decode");
+        match instruction {
+            _ => self.cycle_execute_foo(0, 1, 2),
+        }
+    }
+
+    fn cycle_update_timers(&self) {
+        println!("WRITEME: cycle_update_timers")
+    }
+
+    // OPCODE EXECUTION ////////////////////////////////////////////////////////////////////////////
+
+    fn cycle_execute_foo(&self, x: u8, y: u8, n: u8) {
+        println!("WRITEME: cycle_execute_foo; x:{}, y:{}, n:{}", x, y, n);
     }
 }
 
@@ -70,8 +103,7 @@ pub fn emulate(game_rom: Vec<Byte>) {
     setup_graphics();
     setup_input();
 
-    let chip8 = Chip8::new();
-    chip8.load_game(&game_rom);
+    let chip8 = Chip8::new(&game_rom);
 
     loop {
         chip8.emulate_cycle();
