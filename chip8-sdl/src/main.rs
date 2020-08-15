871baf10b969400edf41ce20ae1c64097a6f9f45
Saverio Miroddi <saverio.pub2@gmail.com>
2020-08-15 23:39:01 +0200

    chip8-sdl: Add debug mode (enables logging)

--- chip8-sdl/src/main.rs
+++ chip8-sdl/src/main.rs
@@ -1,28 +1,43 @@
 use clap::{self, App, Arg};
 
 use frontend_sdl::FrontendSdl;
-use interfaces::NullLogger;
+use interfaces::{Logger, NullLogger, StdoutLogger};
 use libchip8;
 
 use std::error::Error;
 use std::fs;
 
-fn decode_commandline_arguments() -> String {
+fn decode_commandline_arguments() -> (String, bool) {
     let commandline_args = std::env::args().collect::<Vec<String>>();
 
     let matches = App::new("chip8")
         .arg(Arg::with_name("GAME_ROM").required(true).index(1))
+        .arg(
+            Arg::with_name("DEBUG")
+                .short("d")
+                .long("debug")
+                .help("Enable debug mode (logs to stdout)"),
+        )
         .get_matches_from(commandline_args);
 
-    matches.value_of("GAME_ROM").unwrap().to_string()
+    let game_rom_filename = matches.value_of("GAME_ROM").unwrap().to_string();
+    let debug_mode = matches.is_present("DEBUG");
+
+    (game_rom_filename, debug_mode)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let game_rom_filename = decode_commandline_arguments();
+    let (game_rom_filename, debug_mode) = decode_commandline_arguments();
+
     let game_rom_data = fs::read(game_rom_filename)?;
 
     let mut sdl_frontend = FrontendSdl::new("CHIP-8!");
-    let mut logger = NullLogger::new();
+
+    let mut logger: Box<dyn Logger> = if debug_mode {
+        Box::new(StdoutLogger::new())
+    } else {
+        Box::new(NullLogger::new())
+    };
 
     let mut chip8 = libchip8::Chip8::new(&mut sdl_frontend, &mut logger, &game_rom_data);
 
