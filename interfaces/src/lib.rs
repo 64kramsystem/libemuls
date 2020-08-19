9a0c9022e1bfe4b09f8d9dcc09d6204aa4b42e11
Saverio Miroddi <saverio.pub2@gmail.com>
2020-08-19 18:13:21 +0200

    interfaces: Allow `clippy::new_without_default` warning

--- interfaces/src/lib.rs
+++ interfaces/src/lib.rs
@@ -1,3 +1,8 @@
+// From the version (nightly 792c645 2020-08-17). Not really meaningful, as triggered on structs
+// without fields.
+//
+#![allow(clippy::new_without_default)]
+
 mod io_frontend;
 mod keycode;
 
