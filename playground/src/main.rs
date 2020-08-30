b778e3075d70dc96345ac8a3a11ca7c4a857f795
Saverio Miroddi <saverio.pub2@gmail.com>
2020-08-30 13:23:40 +0200

    Project: Add current playground

---
+++ playground/src/main.rs
@@ -0,0 +1,12 @@
+#![allow(unused_must_use)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_assignments)]
+#![allow(unused_mut)]
+// test::bench::black_box
+#![feature(test)]
+
+fn main() {
+    // ...
+}
