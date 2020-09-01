7b089b86e32e0a9bb212c8d156a29dbec7e97e3f
Saverio Miroddi <saverio.pub2@gmail.com>
2020-09-01 02:04:40 +0200

    Playground: Add async approaches to the thread/sleep suite

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -7,9 +7,9 @@
 
 mod archive {
     pub mod _200831_clock_synchronization;
-    pub mod _200831_thread_sleep_overhead;
+    pub mod _200831_sleep_and_concurrency;
 }
 
 fn main() {
-    archive::_200831_clock_synchronization::execute();
+    archive::_200831_sleep_and_concurrency::execute();
 }
