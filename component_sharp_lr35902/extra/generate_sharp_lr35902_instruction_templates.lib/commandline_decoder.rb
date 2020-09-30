f0aa24ebee5f857a4252a929583bf42de9d0564c
Saverio Miroddi <saverio.pub2@gmail.com>
2020-09-30 17:55:52 +0200

    component_sharp_lr35902: geneator: Extend and refactor commandline help

--- component_sharp_lr35902/extra/generate_sharp_lr35902_instruction_templates.lib/commandline_decoder.rb
+++ component_sharp_lr35902/extra/generate_sharp_lr35902_instruction_templates.lib/commandline_decoder.rb
@@ -1,12 +1,18 @@
 module CommandlineDecoder
   extend self
 
+  HELP = <<~HELP
+    Usage: #{File.basename($PROGRAM_NAME)} [<opcode_>{,<opcode_...>}]
+
+    Opcodes are hex, case-independent, and without prefix, e.g. `06,3C`.
+  HELP
+
   def execute
     if (ARGV & %w[-h --help]).size > 0
-      puts "Usage: #{File.basename($PROGRAM_NAME)} [0x<opcode_hex>{,0x<opcode_hex...>}]"
+      puts HELP
       exit 0
     elsif ARGV.size > 1
-      puts "Usage: #{File.basename($PROGRAM_NAME)} [0x<opcode_hex>{,0x<opcode_hex...>}]"
+      puts HELP
       exit 1
     else
       ARGV[0].to_s.split(",").map(&:hex)
